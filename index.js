83e01ca904c31e83e8ac5e6fe35d46780220ed6d
958661CarlBautista <CarlEzekiel.Bautista@edu.sait.ca>
2025-09-09 20:41:27 -0600

    Added Node Command Prompt image

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ function brokenArray(){
       else{
         const msg = document.createElement("div");
         const errorMessage = "Error: Character at index ${i} is missing a 'name' property.";
+        console.error(errorMessage);
         msg.textContent = errorMessage;
         errors.appendChild(msg);  
       }
@@ -103,20 +104,9 @@ function brokenArray(){
   }
 brokenArray();
 
+// Exercise 6: Test with broken data
 
+//const secondBrokenArray =
 
-
-
-// broken test data for exercise 6
-
-// 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
-
-// 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
-
-// 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
-
-// 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
-
-// 5. Add error handling to your functions that will log an error message using console.error() if any object doesn't have a "name" property. Display any error messages in the div with id "error-messages"
 
 // 6. Test your error handling by creating a second array that's intentionally broken (missing name properties) and passing it to your functions. Verify that your error handling works correctly and displays errors in the div with id "broken-array-errors"
